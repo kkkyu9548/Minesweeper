cbf2e4b3eb3e0c4f9b5b8ed0bbc7de62a4bfe8e1
kkkyu9548 <kikiki9548@gmail.com>
2021-05-02 19:47:50 +0900

    코드 수정(테스트)

--- src/com/kkkyu9548/Minesweeper/Main.java
+++ src/com/kkkyu9548/Minesweeper/Main.java
@@ -3,7 +3,10 @@ package com.kkkyu9548.Minesweeper;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Java!!!");
-	// write your code here
+        int i;
+        for(i=0;i<10;i++) {
+            System.out.println("Hello, Java!!!");
+            // write your code here
+        }
     }
 }

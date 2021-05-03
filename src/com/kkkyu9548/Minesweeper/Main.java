c8d1d947a57bc2e2c3a3c3721e7964812ebacb62
kkkyu9548 <kikiki9548@gmail.com>
2021-05-03 16:20:21 +0900

    배열 생성

--- src/com/kkkyu9548/Minesweeper/Main.java
+++ src/com/kkkyu9548/Minesweeper/Main.java
@@ -3,10 +3,25 @@ package com.kkkyu9548.Minesweeper;
 public class Main {
 
     public static void main(String[] args) {
-        int i;
-        for(i=0;i<10;i++) {
-            System.out.println("Hello, Java!!!");
-            // write your code here
-        }
+
+        // 리셋 기능 (미구현)
+
+        int n;//지뢰 난이도 변수
+        int m;//지회 갯수 변수
+
+        //지회찾기 난이도 설정 초급=x, 중급=y, 고급=z
+        //if(난이도 설정 받아오기 디폴트값 초급){
+        //초급 => n=10; m=10;
+        //중급 => n=15; m=20;
+        //고급 => n=20; m=30; 임시값들
+        //}
+        n=10;//임의로 n값 설정(테스트)
+
+        int[][] A = new int[n][n];
+
+        //지뢰 삽입 랜덤하게 배치 (미구현)
+
+        // 배열수 만큼 버튼 생성 (미구현)
+
     }
 }

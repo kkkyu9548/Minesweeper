b05a5dedc0ed28ab47f04441711b4e9a20b384ad
kkkyu9548 <kikiki9548@gmail.com>
2021-05-04 19:05:51 +0900

    테스트 클래스, 지뢰 만들기 클래스 생성

---
+++ src/com/kkkyu9548/Minesweeper/RandomMine.java
@@ -0,0 +1,27 @@
+package com.kkkyu9548.Minesweeper;
+
+public class RandomMine {
+    public static <x, y> void Random(char x, int y){
+        x='1';
+        y=10;
+        if(x == '2'){
+            y=20;
+        }else if(x == '3'){
+            y=30;
+        }
+        return;
+    }
+
+
+//    난이도 값 받아오기
+
+//    String x,y,z = new String();
+//    private int[][] array;
+
+//    외부로 부터 받아와야하는 값 : 난이도 1,2,3
+//    내부에 만들 값 : 난이도별 지뢰생성
+//
+//    지뢰갯수 설정
+//    지뢰위치 생성
+
+}
